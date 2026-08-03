8be84bcbe29d20d3735331a037c4761a50a11b1e
hadi-codes <saiyedhadi847@gmail.com>
2026-08-03 11:14:49 +0530

    Time: 31 ms (38.18%), Space: 27.2 MB (38.18%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.php
@@ -0,0 +1,25 @@
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function maxArea($height) {
+        $left = 0;
+        $right = count($height) - 1;
+        $maxArea = 0;
+
+        while ($left < $right) {
+            $area = min($height[$left], $height[$right]) * ($right - $left);
+            $maxArea = max($maxArea, $area);
+
+            if ($height[$left] < $height[$right]) {
+                $left++;
+            } else {
+                $right--;
+            }
+        }
+
+        return $maxArea;
+    }
+}
