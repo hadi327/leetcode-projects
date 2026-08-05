05b8c922468c02cd3b9ff6d1bb5e8175d1a3b212
hadi-codes <saiyedhadi847@gmail.com>
2026-08-05 11:12:13 +0530

    Time: 0 ms (100%), Space: 42 MB (54.45%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -0,0 +1,25 @@
+/* The guess API is defined in the parent class GuessGame.
+   int guess(int num);
+*/
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int res = guess(mid);
+
+            if (res == 0) {
+                return mid;
+            } else if (res == -1) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
