57bc212cca64b39d168174ccd5e35b4c0f08fc9a
hadi-codes <saiyedhadi847@gmail.com>
2026-08-05 11:47:59 +0530

    Time: 19 ms (47.44%), Space: 77.2 MB (94.4%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    const n = temperatures.length;
+    const ans = new Array(n).fill(0);
+    const stack = [];
+
+    for (let i = 0; i < n; i++) {
+        while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            const idx = stack.pop();
+            ans[idx] = i - idx;
+        }
+
+        stack.push(i);
+    }
+
+    return ans;
+};
