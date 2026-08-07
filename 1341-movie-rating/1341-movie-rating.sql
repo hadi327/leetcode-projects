56e610bc53ca96446006cfccfa0edcdbc0850fce
hadi-codes <saiyedhadi847@gmail.com>
2026-08-07 20:39:04 +0530

    Time: 1503 ms (36.71%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,27 +1,21 @@
-SELECT name AS results
-FROM Users
-WHERE user_id = (
-    SELECT mr.user_id
-    FROM MovieRating mr
-    JOIN Users u
-    ON mr.user_id = u.user_id
-    GROUP BY mr.user_id, u.name
-    ORDER BY COUNT(*) DESC, u.name ASC
+(
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating mr
+        ON u.user_id = mr.user_id
+    GROUP BY u.user_id, u.name
+    ORDER BY COUNT(*) DESC, u.name
     LIMIT 1
 )
-
 UNION ALL
-
-SELECT title AS results
-FROM Movies
-WHERE movie_id = (
-    SELECT mr.movie_id
-    FROM MovieRating mr
-    JOIN Movies m
-    ON mr.movie_id = m.movie_id
-    WHERE created_at >= '2020-02-01'
-      AND created_at < '2020-03-01'
-    GROUP BY mr.movie_id, m.title
-    ORDER BY AVG(rating) DESC, m.title ASC
+(
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN MovieRating mr
+        ON m.movie_id = mr.movie_id
+    WHERE mr.created_at >= '2020-02-01'
+      AND mr.created_at < '2020-03-01'
+    GROUP BY m.movie_id, m.title
+    ORDER BY AVG(mr.rating) DESC, m.title
     LIMIT 1
 );
