66e934e9d023ec8ce62a34f518949f0ba87c433a
hadi-codes <saiyedhadi847@gmail.com>
2026-08-13 10:44:48 +0530

    Time: 62 ms (27.36%), Space: 57.3 MB (87.46%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (k < 2 || nums[i] !== nums[k - 2]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
