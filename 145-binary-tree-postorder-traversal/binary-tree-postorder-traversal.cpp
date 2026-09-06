fc433b0c7bc035fce641db8eb198ee9fead09038
hadi-codes <saiyedhadi847@gmail.com>
2026-09-06 09:23:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (17.85%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        dfs(root, result);
+        return result;
+    }
+
+private:
+    void dfs(TreeNode* node, vector<int>& result) {
+        if (!node)
+            return;
+
+        dfs(node->left, result);
+        dfs(node->right, result);
+        result.push_back(node->val);
+    }
+};
