f435cd2123104233531b68537a15a17fbd4180a9
hadi-codes <saiyedhadi847@gmail.com>
2026-09-06 09:16:06 +0530

    Time: 2 ms (71.44%) | Memory: 12.3 MB (29.37%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+
+    Node* cloneGraph(Node* node) {
+        if (!node)
+            return nullptr;
+
+        if (mp.count(node))
+            return mp[node];
+
+        Node* copy = new Node(node->val);
+        mp[node] = copy;
+
+        for (Node* neighbor : node->neighbors) {
+            copy->neighbors.push_back(cloneGraph(neighbor));
+        }
+
+        return copy;
+    }
+};
