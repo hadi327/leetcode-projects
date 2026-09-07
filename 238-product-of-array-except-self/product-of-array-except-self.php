79ce5e189243603bcca2c5202650f679963cfa2f
hadi-codes <saiyedhadi847@gmail.com>
2026-09-07 08:38:51 +0530

    Time: 7 ms (94.55%) | Memory: 31.6 MB (76.36%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.php
@@ -0,0 +1,22 @@
+class Solution {
+    function productExceptSelf($nums) {
+        $n = count($nums);
+        $result = array_fill(0, $n, 1);
+
+        $prefix = 1;
+
+        for ($i = 0; $i < $n; $i++) {
+            $result[$i] = $prefix;
+            $prefix *= $nums[$i];
+        }
+
+        $suffix = 1;
+
+        for ($i = $n - 1; $i >= 0; $i--) {
+            $result[$i] *= $suffix;
+            $suffix *= $nums[$i];
+        }
+
+        return $result;
+    }
+}
