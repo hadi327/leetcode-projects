077b81821bfb78ac7814d290d258c59e351b2193
hadi-codes <saiyedhadi847@gmail.com>
2026-09-07 08:22:17 +0530

    Time: 16 ms (18.22%) | Memory: 96.8 MB (11.01%) - LeetSync

---
+++ 189-rotate-array/rotate-array.js
@@ -0,0 +1,17 @@
+var rotate = function(nums, k) {
+    k = k % nums.length;
+
+    function reverse(left, right) {
+        while (left < right) {
+            [nums[left], nums[right]] =
+            [nums[right], nums[left]];
+
+            left++;
+            right--;
+        }
+    }
+
+    reverse(0, nums.length - 1);
+    reverse(0, k - 1);
+    reverse(k, nums.length - 1);
+};
