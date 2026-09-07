3dee05286fe977467a1fedcae38cea27f52d501d
hadi-codes <saiyedhadi847@gmail.com>
2026-09-07 08:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (15.00%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.php
@@ -0,0 +1,16 @@
+class Solution {
+    function invertTree($root) {
+        if ($root === null) {
+            return null;
+        }
+
+        $temp = $root->left;
+        $root->left = $root->right;
+        $root->right = $temp;
+
+        $this->invertTree($root->left);
+        $this->invertTree($root->right);
+
+        return $root;
+    }
+}
