85836f5bdeafa241fdadec7daa9e7484bd509881
hadi-codes <saiyedhadi847@gmail.com>
2026-09-07 08:35:41 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (25.00%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.php
@@ -0,0 +1,28 @@
+class Solution {
+    function summaryRanges($nums) {
+        $result = [];
+        $n = count($nums);
+
+        if ($n == 0) {
+            return $result;
+        }
+
+        $start = $nums[0];
+
+        for ($i = 1; $i <= $n; $i++) {
+            if ($i == $n || $nums[$i] != $nums[$i - 1] + 1) {
+                if ($start == $nums[$i - 1]) {
+                    $result[] = (string)$start;
+                } else {
+                    $result[] = $start . "->" . $nums[$i - 1];
+                }
+
+                if ($i < $n) {
+                    $start = $nums[$i];
+                }
+            }
+        }
+
+        return $result;
+    }
+}
