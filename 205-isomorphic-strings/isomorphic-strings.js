95bbcd9c9c26f4fee7201533f0704fa3bc897c7d
hadi-codes <saiyedhadi847@gmail.com>
2026-09-07 08:26:06 +0530

    Time: 6 ms (71.76%) | Memory: 54.6 MB (69.82%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,21 @@
+var isIsomorphic = function(s, t) {
+    const mapST = new Map();
+    const mapTS = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        const a = s[i];
+        const b = t[i];
+
+        if (
+            (mapST.has(a) && mapST.get(a) !== b) ||
+            (mapTS.has(b) && mapTS.get(b) !== a)
+        ) {
+            return false;
+        }
+
+        mapST.set(a, b);
+        mapTS.set(b, a);
+    }
+
+    return true;
+};
