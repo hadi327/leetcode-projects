a5f0f1bdc0ae336c8b41e329ed216f962435f288
hadi-codes <saiyedhadi847@gmail.com>
2026-09-07 08:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (55.86%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.js
@@ -0,0 +1,15 @@
+var convertToTitle = function(columnNumber) {
+    let result = "";
+
+    while (columnNumber > 0) {
+        columnNumber--;
+
+        result =
+            String.fromCharCode(65 + (columnNumber % 26)) +
+            result;
+
+        columnNumber = Math.floor(columnNumber / 26);
+    }
+
+    return result;
+};
