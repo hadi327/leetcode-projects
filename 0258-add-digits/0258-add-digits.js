aeeb44159b697c276ac2857622f40de1ea51789e
hadi-codes <saiyedhadi847@gmail.com>
2026-09-08 08:31:52 +0530

    Time: 0 ms (100%), Space: 54.7 MB (97.17%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.js
@@ -0,0 +1,14 @@
+var addDigits = function(num) {
+    while (num >= 10) {
+        let sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+
+        num = sum;
+    }
+
+    return num;
+};
