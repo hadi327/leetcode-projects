5db537c271f7f744623b508540cd70d54e693605
hadi-codes <saiyedhadi847@gmail.com>
2026-09-09 09:52:51 +0530

    Time: 163 ms (42.47%) | Memory: 67.2 MB (97.70%) - LeetSync

---
+++ 2760-snail-traversal/snail-traversal.js
@@ -0,0 +1,28 @@
+Array.prototype.snail = function(rowsCount, colsCount) {
+    if (rowsCount * colsCount !== this.length) {
+        return [];
+    }
+
+    const result = Array.from(
+        { length: rowsCount },
+        () => Array(colsCount)
+    );
+
+    let index = 0;
+
+    for (let col = 0; col < colsCount; col++) {
+        if (col % 2 === 0) {
+            // Top → Bottom
+            for (let row = 0; row < rowsCount; row++) {
+                result[row][col] = this[index++];
+            }
+        } else {
+            // Bottom → Top
+            for (let row = rowsCount - 1; row >= 0; row--) {
+                result[row][col] = this[index++];
+            }
+        }
+    }
+
+    return result;
+};
