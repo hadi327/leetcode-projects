ede985a42e35b9f2107aa411baac31de96f82613
hadi-codes <saiyedhadi847@gmail.com>
2026-09-11 05:50:27 +0530

    Time: 0 ms (100%), Space: 56.5 MB (45.2%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+
+    let left = 0;
+    let right = height.length - 1;
+
+    let leftMax = 0;
+    let rightMax = 0;
+
+    let water = 0;
+
+    while (left < right) {
+
+        leftMax = Math.max(leftMax, height[left]);
+        rightMax = Math.max(rightMax, height[right]);
+
+        if (leftMax < rightMax) {
+
+            water += leftMax - height[left];
+            left++;
+
+        } else {
+
+            water += rightMax - height[right];
+            right--;
+        }
+    }
+
+    return water;
+};
