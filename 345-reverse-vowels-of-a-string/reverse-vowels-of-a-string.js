1c6b81e904a8059cfa8c60f075f378b8978446b9
hadi-codes <saiyedhadi847@gmail.com>
2026-09-13 06:45:38 +0530

    Time: 10 ms (30.54%) | Memory: 60.8 MB (47.81%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -0,0 +1,31 @@
+var reverseVowels = function(s) {
+    const vowels = new Set("aeiouAEIOU");
+    const arr = s.split("");
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        while (
+            left < right &&
+            !vowels.has(arr[left])
+        ) {
+            left++;
+        }
+
+        while (
+            left < right &&
+            !vowels.has(arr[right])
+        ) {
+            right--;
+        }
+
+        [arr[left], arr[right]] =
+            [arr[right], arr[left]];
+
+        left++;
+        right--;
+    }
+
+    return arr.join("");
+};
