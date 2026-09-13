015baf53e8787610da105c3eb05048fa0379bf73
hadi-codes <saiyedhadi847@gmail.com>
2026-09-13 06:52:36 +0530

    Time: 18 ms (78.57%) | Memory: 56.9 MB (54.08%) - LeetSync

---
+++ 368-largest-divisible-subset/largest-divisible-subset.js
@@ -0,0 +1,34 @@
+var largestDivisibleSubset = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    const n = nums.length;
+    const dp = new Array(n).fill(1);
+    const prev = new Array(n).fill(-1);
+
+    let maxIndex = 0;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (
+                nums[i] % nums[j] === 0 &&
+                dp[j] + 1 > dp[i]
+            ) {
+                dp[i] = dp[j] + 1;
+                prev[i] = j;
+            }
+        }
+
+        if (dp[i] > dp[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    const result = [];
+
+    while (maxIndex !== -1) {
+        result.push(nums[maxIndex]);
+        maxIndex = prev[maxIndex];
+    }
+
+    return result.reverse();
+};
