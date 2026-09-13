03a7352905b05cce2e0031c9c3b840b7ec64c38c
hadi-codes <saiyedhadi847@gmail.com>
2026-09-13 06:46:29 +0530

    Time: 1 ms (77.07%) | Memory: 56.6 MB (13.37%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -0,0 +1,13 @@
+var intersection = function(nums1, nums2) {
+    const set1 = new Set(nums1);
+    const result = [];
+
+    for (const num of nums2) {
+        if (set1.has(num)) {
+            result.push(num);
+            set1.delete(num);
+        }
+    }
+
+    return result;
+};
