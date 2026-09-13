04db3178e0273edac0a01fb571cff06ada0fd9a0
hadi-codes <saiyedhadi847@gmail.com>
2026-09-13 07:03:58 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (61.90%) - LeetSync

---
+++ 376-wiggle-subsequence/wiggle-subsequence.js
@@ -0,0 +1,18 @@
+var wiggleMaxLength = function(nums) {
+    if (nums.length <= 1) {
+        return nums.length;
+    }
+
+    let up = 1;
+    let down = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            up = down + 1;
+        } else if (nums[i] < nums[i - 1]) {
+            down = up + 1;
+        }
+    }
+
+    return Math.max(up, down);
+};
