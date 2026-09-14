5eb21ffca05c5a7349d176c8f1aa3e8b06ed682e
hadi-codes <saiyedhadi847@gmail.com>
2026-09-14 06:07:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (84.34%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        if (nums.size() < 3) {
+            return 0;
+        }
+
+        int current = 0;
+        int result = 0;
+
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] - nums[i - 1] ==
+                nums[i - 1] - nums[i - 2]) {
+                current++;
+                result += current;
+            } else {
+                current = 0;
+            }
+        }
+
+        return result;
+    }
+};
