32b62b868b65ef52acbb75c3e456abfdda66110b
hadi-codes <saiyedhadi847@gmail.com>
2026-09-15 07:32:18 +0530

    Time: 3 ms (94.65%) | Memory: 44.8 MB (78.49%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findContentChildren(
+        vector<int>& g,
+        vector<int>& s
+    ) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int child = 0;
+
+        for (int cookie : s) {
+            if (child < g.size() &&
+                cookie >= g[child]) {
+                child++;
+            }
+        }
+
+        return child;
+    }
+};
