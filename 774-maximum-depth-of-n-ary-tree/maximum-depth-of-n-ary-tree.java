e5b3229793bee2873964a49abd55606a9c18403c
hadi-codes <saiyedhadi847@gmail.com>
2026-09-17 06:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (25.17%) - LeetSync

---
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null)
+            return 0;
+
+        int depth = 0;
+
+        for (Node child : root.children)
+            depth = Math.max(depth, maxDepth(child));
+
+        return depth + 1;
+    }
+}
