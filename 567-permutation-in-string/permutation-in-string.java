0a44dc89da5363ce1117ffd03bb715aea51becb3
hadi-codes <saiyedhadi847@gmail.com>
2026-09-17 06:07:33 +0530

    Time: 7 ms (66.92%) | Memory: 43.8 MB (92.93%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+
+        int[] count = new int[26];
+
+        for (char c : s1.toCharArray())
+            count[c - 'a']++;
+
+        int left = 0;
+
+        for (int right = 0; right < s2.length(); right++) {
+            count[s2.charAt(right) - 'a']--;
+
+            if (right - left + 1 > s1.length()) {
+                count[s2.charAt(left) - 'a']++;
+                left++;
+            }
+
+            if (right - left + 1 == s1.length() &&
+                allZero(count)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean allZero(int[] count) {
+        for (int value : count) {
+            if (value != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
