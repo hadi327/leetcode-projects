2506e35eea442086916275606123ec93c980e293
hadi-codes <saiyedhadi847@gmail.com>
2026-09-18 07:55:57 +0530

    Time: 6 ms (27.92%) | Memory: 24.4 MB (77.68%) - LeetSync

---
+++ 859-design-circular-deque/design-circular-deque.cpp
@@ -0,0 +1,81 @@
+class MyCircularDeque {
+private:
+    vector<int> dq;
+    int front;
+    int rear;
+    int size;
+    int capacity;
+
+public:
+    MyCircularDeque(int k) {
+        dq.resize(k);
+        front = 0;
+        rear = 0;
+        size = 0;
+        capacity = k;
+    }
+
+    bool insertFront(int value) {
+        if (isFull())
+            return false;
+
+        front = (front - 1 + capacity) % capacity;
+        dq[front] = value;
+        size++;
+
+        return true;
+    }
+
+    bool insertLast(int value) {
+        if (isFull())
+            return false;
+
+        dq[rear] = value;
+        rear = (rear + 1) % capacity;
+        size++;
+
+        return true;
+    }
+
+    bool deleteFront() {
+        if (isEmpty())
+            return false;
+
+        front = (front + 1) % capacity;
+        size--;
+
+        return true;
+    }
+
+    bool deleteLast() {
+        if (isEmpty())
+            return false;
+
+        rear = (rear - 1 + capacity) % capacity;
+        size--;
+
+        return true;
+    }
+
+    int getFront() {
+        if (isEmpty())
+            return -1;
+
+        return dq[front];
+    }
+
+    int getRear() {
+        if (isEmpty())
+            return -1;
+
+        return dq[(rear - 1 + capacity) % capacity];
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    bool isFull() {
+        return size == capacity;
+    }
+};
