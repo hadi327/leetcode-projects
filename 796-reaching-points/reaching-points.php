2062513db58e5f2ff0a983fe97b7f0299bdcd1f2
hadi-codes <saiyedhadi847@gmail.com>
2026-09-19 08:11:55 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (100.00%) - LeetSync

---
+++ 796-reaching-points/reaching-points.php
@@ -0,0 +1,26 @@
+class Solution {
+
+    function reachingPoints($sx, $sy, $tx, $ty) {
+        while ($tx >= $sx && $ty >= $sy) {
+            if ($tx == $sx && $ty == $sy) {
+                return true;
+            }
+
+            if ($tx > $ty) {
+                if ($ty == $sy) {
+                    return ($tx - $sx) % $ty == 0;
+                }
+
+                $tx %= $ty;
+            } else {
+                if ($tx == $sx) {
+                    return ($ty - $sy) % $tx == 0;
+                }
+
+                $ty %= $tx;
+            }
+        }
+
+        return false;
+    }
+}
