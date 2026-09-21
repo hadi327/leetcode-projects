b26545961efd50855870417dadcca461d784db95
hadi-codes <saiyedhadi847@gmail.com>
2026-09-21 06:09:15 +0530

    Time: 23 ms (80.91%) | Memory: 57.4 MB (67.27%) - LeetSync

---
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.js
@@ -0,0 +1,15 @@
+var largestPerimeter = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    for (let i = nums.length - 1; i >= 2; i--) {
+        const a = nums[i - 2];
+        const b = nums[i - 1];
+        const c = nums[i];
+
+        if (a + b > c) {
+            return a + b + c;
+        }
+    }
+
+    return 0;
+};
