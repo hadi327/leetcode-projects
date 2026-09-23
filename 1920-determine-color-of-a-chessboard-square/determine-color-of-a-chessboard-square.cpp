533914df24289699fa47a58b37c1d12ee6d0bcee
hadi-codes <saiyedhadi847@gmail.com>
2026-09-23 06:19:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (46.59%) - LeetSync

---
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        int column = coordinates[0] - 'a' + 1;
+        int row = coordinates[1] - '0';
+
+        return (column + row) % 2 == 1;
+    }
+};
