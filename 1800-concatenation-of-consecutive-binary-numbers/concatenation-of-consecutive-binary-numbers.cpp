214a44fbee70572b10e5344c8756c06e1b44620d
hadi-codes <saiyedhadi847@gmail.com>
2026-09-23 06:06:30 +0530

    Time: 27 ms (70.04%) | Memory: 8.1 MB (67.41%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const long long MOD = 1e9 + 7;
+
+        long long ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int bits = 32 - __builtin_clz(i);
+
+            ans = ((ans << bits) + i) % MOD;
+        }
+
+        return ans;
+    }
+};
